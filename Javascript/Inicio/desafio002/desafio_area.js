cc52a7b8dccb32e2bf28b309f5e3fbaa26af153f
LordRyanII <ryanoliveiram2020@outlook.com>
2022-09-27 21:41:10 -0300

    Hello Word!

--- Javascript/Inicio/desafio002/desafio_area.js
+++ Javascript/Inicio/desafio002/desafio_area.js
@@ -6,4 +6,4 @@ let raio = 10
 
 const res = PI * (10 * 2)
 
-console.log(`O valor de  pi é ${PI}, e o nosso raio é ${raio * 2} e o total é ${res}`)
+console.log(`O valor de  pi é ${PI}, e o nosso raio é ${raio * 2} e o total é ${res}` + 'm2')
---
+++ Javascript/fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+//bloco:
+{
+    let word = 'Olá, Mundo!'
+    console.log('Podemos organizar um código dessa meneira')
+    for(let c = 1; c < 10; c++){
+        console.log(word)
+    } console.log('Estou livre da maldição')
+}
