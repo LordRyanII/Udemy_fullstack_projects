b849b1bda96447c603543f1f38a685fa78f64130
LordRyanII <ryanoliveiram2020@outlook.com>
2022-09-28 20:59:16 -0300

    Template String

---
+++ Javascript/fundamentos/templateString.js
@@ -0,0 +1,14 @@
+//Formatando_strings
+
+let Name = 'R3nata'
+const sobrenome = 'Alves Da Luz'
+
+console.log(Name.charAt(0)) //mostra as letras do nome atribuído na variavel
+console.log(Name.charAt(1))
+console.log(Name.charAt(2))
+console.log(Name.charAt(3))
+console.log(Name.charAt(4))
+console.log(Name.charAt(5))
+
+console.log(Name.replace(1, 'e').toString())
+console.log(`Nome completo: ${Name} ` + sobrenome)
