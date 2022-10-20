3781890ad8537b23bb2cdfa9a9d064f3887e1084
LordRyanII <ryanoliveiram2020@outlook.com>
2022-10-20 20:46:34 -0300

    Aprendendo sobre Atribuição e hosting

--- Javascript/fundamentos/QuaseTudoeFuncao.js
+++ Javascript/fundamentos/QuaseTudoeFuncao.js
@@ -4,9 +4,9 @@ let hora = agora.getHours()
 console.log(hora)
 
 if(hora <= 12){
-    console.log(`Agora são ${hora} da manhã`)
-} else if(hora >= 12 || hora <= 18){
-    console.log(`Agora são ${hora} da tarde`)
-}else{
-    console.log(`Agora são ${hora} da noite`)
+    console.log(`Agora são ${hora} horas da manhã`)
+} else if(hora >= 12 && hora <= 18){
+    console.log(`Agora são ${hora} horas da tarde`)
+}else if(hora >= 18 && hora <= 23){
+    console.log(`Agora são ${hora} horas da noite`)
 }
---
+++ Javascript/fundamentos/atribuição.js
@@ -0,0 +1,7 @@
+//Mostrando formas de atribuição
+
+let Number_1 = 50
+let Number_2 = 35
+
+const res = Number_1 -= Number_2
+console.log(res)
