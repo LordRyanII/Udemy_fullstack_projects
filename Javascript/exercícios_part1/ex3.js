7041538de73f813cf2b104530a61359f62c640b4
LordRyanII <ryanoliveiram2020@outlook.com>
2023-01-15 16:41:20 -0300

    Exercicío_3
    Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.

---
+++ Javascript/exercícios_part1/ex3.js
@@ -0,0 +1,9 @@
+/*
+    Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+
+function potencia(n1, n2){
+    return n1 ** n2
+}
+
+console.log(potencia(3, 2))
