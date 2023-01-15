1d3f1ac750a6d4f47d049a8e538e85b7a5441dae
LordRyanII <ryanoliveiram2020@outlook.com>
2023-01-15 16:58:58 -0300

    Exercício_4
    Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
    e o resto da divisão destes dois valores.

---
+++ Javascript/exercícios_part1/ex4.js
@@ -0,0 +1,10 @@
+/*Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.*/
+
+function potencia(n1, n2){
+    return `A divisão de ${n1} e ${n2} é iguaal a:
+    ${n1 / n2} com resto: ${n1 % n2}`
+}
+
+console.log(potencia(3, 1));
+console.log(potencia(50, 20));
