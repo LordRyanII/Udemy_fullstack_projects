b202bb1b6b6753fbcc02c7197d8f6f8d5acdabd4
LordRyanII <ryanoliveiram2020@outlook.com>
2023-01-25 15:48:32 -0300

    Exercicio_25
    Escrever um programa para exibir os números de 1 até 50 na tela.

---
+++ Javascript/exercícios_part1/ex25.js
@@ -0,0 +1,14 @@
+/*
+Escrever um programa para exibir os números de 1 até 50 na tela.
+
+*/
+
+
+const Imprima = () =>{
+    for(let i = 0; i < 51; i++){
+        console.log(`Numero: ` + i)
+    }
+    console.log('Fim! :)')
+}
+
+Imprima()
