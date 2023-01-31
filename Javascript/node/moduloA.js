c08d7c8fe883514acbfd54e3b1252909b061a16a
LordRyanII <ryanoliveiram2020@outlook.com>
2023-01-30 23:29:23 -0300

    Aprendendo Sobre Node.js

---
+++ Javascript/node/moduloA.js
@@ -0,0 +1,9 @@
+// Introdução Ao Node.js
+
+this.ola = 'Olá' //this em node é um objeto, que agora possuí o valor 'Olá'
+exports.bemvindo = 'Bem-vindo ao Node.js' //exports é um objeto
+
+//Formas de exportação:
+
+exports.bemvindo = 'Bem-vindo ao Node.js' //Também é uma forma de exportação
+module.exports.atelogo = 'Até logo' //Também é uma forma de exportação. Forma Clássica
