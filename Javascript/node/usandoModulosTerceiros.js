20e84c62a75f2b402ff1edc8e399836782618931
LordRyanII <ryanoliveiram2020@outlook.com>
2023-01-31 21:01:50 -0300

    Importando bibliotecas de terceiros
    Introdução aos metódos de importação de bibliotecas.

---
+++ Javascript/node/usandoModulosTerceiros.js
@@ -0,0 +1,13 @@
+//Instalando biblioteca de terceiros
+/*
+    Biblioteca de exemplo lodash
+
+    resumo sobre a biblioteca:
+            É interessante ressaltar que os métodos disponibilizados por essa biblioteca sempre iniciam com os caracteres “ _.” seguidos pelo nome da respectiva função. Abaixo, conheça o funcionamento de algumas das principais funções da Lodash!
+*/
+
+const _ = require('lodash');
+
+setInterval(() =>{
+    console.log(_.random(50, 60)) //chamada da função _.random da biblioteca lodash
+}, 2000)
