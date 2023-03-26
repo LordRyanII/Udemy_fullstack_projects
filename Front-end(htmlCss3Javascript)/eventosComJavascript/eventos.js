423110d8835b9dadbf3603e9f762eb7bfedbff8e
LordRyanII <ryanoliveiram2020@outlook.com>
2023-03-26 19:06:57 -0300

    Eventos Javascript

---
+++ Front-end(htmlCss3Javascript)/eventosComJavascript/eventos.js
@@ -0,0 +1,14 @@
+//Sart
+
+function acao(){
+    alert('Deu bom!')
+    
+}
+function hover(){
+    let div = document.querySelector('.quadrado').style.backgroundColor = 'red'
+    
+}
+function sair(){
+    let div = document.querySelector('.quadrado').style.backgroundColor = 'grey'
+    
+}
