43b918fffa6996cf0a1c38125d5eb3d0e0486fef
LordRyanII <ryanoliveiram2020@outlook.com>
2023-03-28 23:29:56 -0300

    Usando construtores

--- Front-end(htmlCss3Javascript)/Javascript/Poo(paradigmaDeOrientacaoaObjeto)/exemploOO.js
+++ Front-end(htmlCss3Javascript)/Javascript/Poo(paradigmaDeOrientacaoaObjeto)/exemploOO.js
@@ -2,15 +2,20 @@ class time {
     constructor(){
         const date = new Date()
 
-        this.hora = date.getHours()
+        this.hora = date.getHours(); //é uma variavél
+        this.minutos = date.getMinutes() //é uma variavél
     }
     displayTime(){
-        if(this.hora > 18){
-            return console.log(`Agora são ${this.hora} da noite`)
+        if(this.hora > 0 && this.hora < 12){
+            return console.log(`Agora são ${this.hora} horas e ${this.minutos} minutos da manhã`)
+        } else if(this.hora > 12 && this.hora < 18){
+            return console.log(`Agora são ${this.hora} horas e ${this.minutos} minutos da tarde`)
+        } else {
+            return console.log(`Agora são ${this.hora} horas e ${this.minutos} minutos da noite`)
         }
     }
 }
 
 let horas = new time()
 
-console.log(horas)
+console.log(horas.displayTime())
