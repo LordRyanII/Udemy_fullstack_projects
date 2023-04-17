0364a06293e9387ccefdc53bf1daa087a4d322ea
LordRyanII <ryanoliveiram2020@outlook.com>
2023-04-17 18:22:50 -0300

    Resolvendo exercicios de lógica de Programação

---
+++ Front-end(htmlCss3Javascript)/Javascript/Exercicios_logicaProgramacao/ex01.js
@@ -0,0 +1,13 @@
+// Escreva uma função que receba dois números e retorne o maior deles
+
+
+function RecebeNumbers(n1, n2){
+    if(n1 > n2){
+        return n1
+    } else {
+        return n2
+    }
+}
+
+
+console.log(RecebeNumbers(10, 20))
