fa479e5018a479d4c1cbc4c8f4263c7c2224dcaf
LordRyanII <ryanoliveiram2020@outlook.com>
2023-04-17 18:28:34 -0300

    Resolvendo exercicio 2 de lógica

---
+++ Front-end(htmlCss3Javascript)/Javascript/Exercicios_logicaProgramacao/ex02.js
@@ -0,0 +1,10 @@
+// Execute uma função que receba o nome epaisagem que recebe dois valores (largura e altura)
+// Retorne true se a 'imagem' estiver no modo paisagem
+
+
+function epaisagem(largura, altura){
+    return largura > altura ? 'true' : 'false'
+}
+
+
+console.log(epaisagem(1920, 1080))
