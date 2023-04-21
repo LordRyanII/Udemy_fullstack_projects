2066af22eeb1ec846b4bb71cb1c80c1bc6a81d22
LordRyanII <ryanoliveiram2020@outlook.com>
2023-04-21 03:13:12 -0300

    Passando arrays

--- Front-end(htmlCss3Javascript)/Javascript/função/ParametroDestructuring.js
+++ Front-end(htmlCss3Javascript)/Javascript/função/ParametroDestructuring.js
@@ -1,8 +1,21 @@
 // Podemos passar objetos literais, por meio da destruturação
-
+/*A
 function dados({ nome, sobrenome, idade}){
 
     return console.log(nome, sobrenome, idade)
 }
 
-dados({ nome:'Ryan', sobrenome:'Menezes', idade: 20})
+const obj = {
+    nome: 'Renata',
+    sobrenome: 'Alves Da Luz',
+    idade: 20
+}
+dados(obj)
+*/
+
+function Array([ valor1, valor2, valor3 ]){
+
+    return console.log(valor1, valor2, valor3)
+}
+
+Array(['Cacho de uva,', 'Pé de banana,', 'Raízes de mandioca'])
