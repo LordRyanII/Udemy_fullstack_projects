b7eac2c8c998250b8d156568ee643c0fe3740cdb
LordRyanII <ryanoliveiram2020@outlook.com>
2023-04-23 19:21:27 -0300

    Funções factory

--- Front-end(htmlCss3Javascript)/Javascript/função/functionFactory.js
+++ Front-end(htmlCss3Javascript)/Javascript/função/functionFactory.js
@@ -1,11 +1,23 @@
 //São funções que retornam objetos
 
-function CriarPessoa(){
-    return {
-        Nome: 'Renata',
-        Sobrenome: 'Alves Da Luz'
+function CriarPessoa(nome, sobrenome, idade){
+    
+    let dados = {
+        nome: nome, 
+        sobrenome: sobrenome, 
+        idade: idade
+    }
+
+    if(nome.length > 10){
+
+        return `${nome}, possuí mais de dez letras, total de letras: ${nome.length}`
+    } else{
+        return `${nome}, possuí menos de dez letras, total de letras: ${nome.length}`
     }
 }
 
-console.log(CriarPessoa())
+let dados1 = CriarPessoa('Joaquim Antônio', 'Alves', 20);
+let dados2 = CriarPessoa('Renata', 'Alves', 20)
 
+console.log(dados1); // Saída: Joaquim Antônio, possuí mais de dez letras, total de letras: 15
+console.log(dados2); //Saída: Renata, possuí menos de dez letras, total de letras: 6
