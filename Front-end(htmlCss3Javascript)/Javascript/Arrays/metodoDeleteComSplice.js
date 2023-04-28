f40b6687da13f0fbecf4ee197573a2d7eaed022a
LordRyanII <ryanoliveiram2020@outlook.com>
2023-04-27 22:15:18 -0300

    metódos para deletar arrays

---
+++ Front-end(htmlCss3Javascript)/Javascript/Arrays/metodoDeleteComSplice.js
@@ -0,0 +1,4 @@
+let myArray = [1, 2, 3, 4, 5];
+
+myArray.splice(2, 1); // remove o elemento 3 do índice 2
+console.log(myArray); // output: [1, 2, 4, 5]
---
+++ Front-end(htmlCss3Javascript)/Javascript/Arrays/metodoShift.js
@@ -0,0 +1,6 @@
+const listaDeCarros = ['Mercedes', 'Ferrari', 'Lamborghini', 'Chevette'];
+
+listaDeCarros.shift();
+
+//retorna o o array sem o primeiro elemento
+console.log(listaDeCarros)
