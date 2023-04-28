bacb68a92deb0b52d2cd4b1c04834a3bcdd2eb6a
LordRyanII <ryanoliveiram2020@outlook.com>
2023-04-27 23:05:00 -0300

    Metódo delete item of array

---
+++ Front-end(htmlCss3Javascript)/Javascript/Arrays/metodoDelete.js
@@ -0,0 +1,9 @@
+const objetos = [{nome: 'Renata', idade: 20 + ' anos'}, {nome: 'Pedro Silva', idade: 35 + ' anos'}];
+
+//Aqui é o Array contendo os dois objetos em sua forma normal
+console.log(objetos);
+
+//Aqui é o metódo deletando um dos Arrays:
+delete objetos[1];
+
+console.log(objetos);
