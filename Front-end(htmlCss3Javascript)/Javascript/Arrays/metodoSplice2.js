5a6aa47e6d332d285dd7be5ca0aa9addc0dff52e
LordRyanII <ryanoliveiram2020@outlook.com>
2023-04-28 23:48:08 -0300

    Splice part2

---
+++ Front-end(htmlCss3Javascript)/Javascript/Arrays/metodoSplice2.js
@@ -0,0 +1,11 @@
+
+//               0        1       2           3         4
+const nome = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Julia'];
+
+//nome.splice(indice que vamos começar, o número de elementos que vamos deletar,elementos que vamos add elemento1);
+
+const ArraysRemovidos = nome.splice(4, 1);
+
+console.log(ArraysRemovidos)
+
+
