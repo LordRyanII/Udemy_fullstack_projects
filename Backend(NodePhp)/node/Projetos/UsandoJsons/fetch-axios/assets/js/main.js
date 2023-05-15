19a5ccc9132aa27689cac2a34bf590870e161ca7
LordRyanII <ryanoliveiram2020@outlook.com>
2023-05-14 23:54:35 -0300

    Comentando algoritmos

--- Backend(NodePhp)/node/Projetos/UsandoJsons/fetch-axios/assets/js/main.js
+++ Backend(NodePhp)/node/Projetos/UsandoJsons/fetch-axios/assets/js/main.js
@@ -1,17 +1,17 @@
 fetch('pessoas.json')
-  .then(resposta => resposta.json()
-  .then(json => {
+  .then(resposta => resposta.json() //Aqui aguardamos uma promise de json
+  .then(json => { //pegamos essa promise e jogamos em uma função
     return carregaDados(json)
   }));
 
-function carregaDados(json){
-  const table = document.querySelector('.table');
+function carregaDados(json){ //Aqui tratamos os dados retornados da função
+  const table = document.querySelector('.table');  //Pegamos a tabela através do DOM
 
-  for(let pessoa of json) {
-    const tr = document.createElement('tr');
-    let td1 = document.createElement('td');
-    td1.innerHTML = pessoa.nome;
-    tr.appendChild(td1);
+  for(let pessoa of json) {  //Criamos as interações, adicionamos cada dado na tabela
+    const tr = document.createElement('tr'); // Criamos uma linha vertical, aonde colocaremos os dados
+    let td1 = document.createElement('td'); // O td é a "célula" aonde o dado ficará
+    td1.innerHTML = pessoa.nome; // escrevemos nessa "célula" o nome da pessoa do json
+    tr.appendChild(td1); //Dizemos que a "célula" é "filho" da linha vertical, ou seja pertence aquela linha
 
     let td3 = document.createElement('td');
     td3.innerHTML = pessoa.idade;
@@ -26,10 +26,10 @@ function carregaDados(json){
     tr.appendChild(td5);
 
     
-    table.appendChild(tr)
+    table.appendChild(tr) // Após adicionamos todas as linhas verticais com os dados na nossa tabela
 
   }
-  const resultado = document.querySelector('.resultado');
-  resultado.appendChild(table)
+  const resultado = document.querySelector('.resultado'); //Adicionamos na nossa div os dados 
+  resultado.appendChild(table); //Dizemos que a "tabela" é "filho" da nossa div resultado
 
 }
