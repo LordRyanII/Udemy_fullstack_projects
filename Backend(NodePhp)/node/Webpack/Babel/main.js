0c9e1fe325049de7c135a346701a054f59c48fb8
LordRyanII <ryanoliveiram2020@outlook.com>
2023-05-16 00:44:56 -0300

    Usando o babel

--- Backend(NodePhp)/node/Webpack/Babel/main.js
+++ Backend(NodePhp)/node/Webpack/Babel/main.js
@@ -3,3 +3,10 @@ const obj = {nome};
 const novoObj = {...obj};
 
 console.log(novoObj);
+
+const data = () => {
+    const date = new Date();
+    return date.getFullYear();
+};
+
+console.log(data());
