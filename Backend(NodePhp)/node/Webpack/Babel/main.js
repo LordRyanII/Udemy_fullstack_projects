031015bcca2a35d4a7fea55d16f6a7e8d3052660
LordRyanII <ryanoliveiram2020@outlook.com>
2023-05-16 00:54:33 -0300

    Testando babel

--- Backend(NodePhp)/node/Webpack/Babel/main.js
+++ Backend(NodePhp)/node/Webpack/Babel/main.js
@@ -1,12 +1,29 @@
 const nome = 'Ryan';
-const obj = {nome};
-const novoObj = {...obj};
+const obj = { nome };
+const novoObj = { ...obj };
 
 console.log(novoObj);
 
-const data = () => {
+const GeraAno = (obj) => {
     const date = new Date();
-    return date.getFullYear();
+    obj = date.getFullYear();
+    return console.log(obj);
 };
 
-console.log(data());
+
+const Gerames = () => {
+    const normalizaMes = () => {
+        const date = new Date();
+        const month = date.getMonth();
+        if (month === 4) {
+            return 'maio';
+        }
+        return obj;
+    };
+
+    return console.log(normalizaMes);
+};
+
+Gerames();
+
+GeraAno();
