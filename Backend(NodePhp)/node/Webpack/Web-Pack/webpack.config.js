95270d6dcb706e000ee300cad24edc8a7e8669ed
LordRyanII <ryanoliveiram2020@outlook.com>
2023-05-18 22:50:26 -0300

    WebPack configurando

--- Backend(NodePhp)/node/Webpack/Web-Pack/webpack.config.js
+++ Backend(NodePhp)/node/Webpack/Web-Pack/webpack.config.js
@@ -1,5 +1,14 @@
 const path = require('path'); //communonJS
 
 module.exports = {
-    mode: 'development'
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map'
+
 };
+
+/* module: {},*/
