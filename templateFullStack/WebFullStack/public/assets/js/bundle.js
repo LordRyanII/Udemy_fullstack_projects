0f2ba8a7b2d7782495fc36a95e262a46b5edfdc7
LordRyanII <ryanoliveiram2020@outlook.com>
2023-05-20 22:10:03 -0300

    Definindo Templates

---
+++ templateFullStack/WebFullStack/public/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+function Soma(n1, n2) {
+  return n1 + n2;
+}
+;
+console.log(Soma(13, 50));
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
