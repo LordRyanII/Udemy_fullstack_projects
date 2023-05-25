98704be3da8711ebaf629cb783c92fceffeba876
LordRyanII <ryanoliveiram2020@outlook.com>
2023-05-25 00:18:04 -0300

    Filename e dirname

---
+++ Backend(NodePhp)/node/introducao2/aula2/FileEDirname.js
@@ -0,0 +1,4 @@
+// Exemplo com filename e dirname
+
+console.log(__dirname); //retorna o diretório do arquivo atualmente em execução.
+console.log(__filename); //retorna o caminho completo do arquivo atualmente em execução
