ac99b96dc81baee0a18154b891c7685ed02d32ac
LordRyanII <ryanoliveiram2020@outlook.com>
2023-06-09 22:18:14 -0300

    Lógica de programação

---
+++ Front-end(htmlCss3Javascript)/Javascript/Exercicios_logicaProgramacao/ex11.js
@@ -0,0 +1,27 @@
+class contaBancaria {
+    constructor(cc, saldo) {
+        this.conta = cc;
+        this.total = saldo;
+        console.log(`Conta: ${this.conta}, Saldo: R$${this.total}`);
+    }
+
+    AdicionaSaldo(valor) {
+        this.total += valor; // Atualiza o saldo adicionando o valor fornecido
+        console.log(`SUCESSO! Você acrescentou o valor de: R$${valor}, total: R$${this.total}`);
+        return this.total; //Atualiza o metódo this.total
+    }
+    Saque(valor) {
+        this.total -= valor; //Atualia o saldo removendo o valor
+        console.log(`SUCESSO! Você fez um saque no valor de: R$${valor}, total: R$${this.total}`)
+        return this.total
+    }
+    Extrato() {
+        console.log(`Conta: ${this.conta}, Saldo: R$${this.total}`);
+    }
+}
+
+const conta = new contaBancaria(1001, 1500);
+conta.AdicionaSaldo(800);
+conta.Extrato();
+conta.Saque(800);
+conta.Extrato();
