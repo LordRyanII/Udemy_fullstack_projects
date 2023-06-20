2faa5157cc02cfa173c6ab1ae75752aed179728a
LordRyanII <ryanoliveiram2020@outlook.com>
2023-06-20 00:46:16 -0300

    Conectando ao mongoDB

--- Backend(NodePhp)/node/introducao2/aula13-BackendComMongoDb/server.js
+++ Backend(NodePhp)/node/introducao2/aula13-BackendComMongoDb/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const connectionString = 'mongodb+srv://ryanmenezes:usuarior@cursojs01.jz8j9mg.mongodb.net/BASEDEDADOS'
+mongoose.connect(connectionString);
 const routes = require('./routes');
 const meuMiddleware = require('./src/middlewares/meuMiddleware.js') //ou const  {meuMiddleware} = require('/src...')
 const path = require('path');
-const porta = 5002
+const porta = 5005
 app.use(express.urlencoded({ extended: true })); //tratar corpo da requisição
 
 //Trata de rotas
-app.use(express.static(path.resolve(__dirname, 'public'))); 
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
