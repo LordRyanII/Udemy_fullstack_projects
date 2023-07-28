bdf362223d5f003add729add375bb00d3c63c943
LordRyanII <ryanoliveiram2020@outlook.com>
2023-07-27 21:27:24 -0300

    Criando rotas api

--- Backend(NodePhp)/node/exerciciosBackend/Api_exercicio/backend/src/controller/produtosController.js
+++ Backend(NodePhp)/node/exerciciosBackend/Api_exercicio/backend/src/controller/produtosController.js
@@ -1,26 +1,45 @@
 class Produtos {
     constructor(...produtos) {
         this._produtos = produtos;
+        this.id = 0
+        
     }
+
     listarProdutos() {
+        const idProdutos = this.id ++
         const produtos = {
-            _id: 1234567890,
+            _id: idProdutos,
             produtos: this._produtos // Renomeie o campo para "produtos"
         };
         return produtos;
     }
+
+    dadosVazios() {
+        return {
+            message: 'Não existem registros no banco.'
+        };
+    }
 }
+
+let produtosCriados = []; // Variável para armazenar os produtos criados
 
 // Função para listar os produtos
 exports.listarProdutos = (req, res, next) => {
-    const produtos = new Produtos();
-    return res.send(produtos.listarProdutos());
+    const produtos = new Produtos(...produtosCriados); // Passa a lista de produtos criados como argumento
+    if (produtosCriados.length === 0) {
+        return res.send(produtos.dadosVazios());
+    } else {
+        return res.send(produtos.listarProdutos());
+    }
 };
+
 
 // Função para criar um novo produto
 exports.criarDados = (req, res) => {
     const { name } = req.body;
     const produtos = new Produtos(name); // Se estiver criando um novo produto, precisa passar o nome como parâmetro
-    res.status(200).send('Success: code 200'); // Enviando uma resposta de sucesso
-};
+    produtosCriados.push(...produtos._produtos); // Adiciona os produtos criados à variável de produtosCriados
+    res.status(200).send({Success: 'code 200'}); // Enviando uma resposta de sucesso
 
+    return produtos;
+};
