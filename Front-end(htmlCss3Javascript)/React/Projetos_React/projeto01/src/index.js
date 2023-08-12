2ffc85ca633693311db9898960855eb40d70481a
LordRyanII <ryanoliveiram2020@outlook.com>
2023-08-12 18:21:21 -0300

    Componentes react

---
+++ Front-end(htmlCss3Javascript)/React/Projetos_React/projeto01/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Primeiro from './componentes/primeiroComponente';
+
+
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
