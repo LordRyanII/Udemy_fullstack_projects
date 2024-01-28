8b5ab25bca9e97b3e67f2a73d2f5a2afd0d35807
LordRyanII <ryanoliveiram2020@outlook.com>
2024-01-27 23:32:17 -0300

    Aprendendo structural type

--- Backend(NodePhp)/node/Typescript/Aulas/Aula14-StructuralTypes/Structural.ts
+++ Backend(NodePhp)/node/Typescript/Aulas/Aula14-StructuralTypes/Structural.ts
@@ -1,1 +1,16 @@
 type verifyUserFn = (user: User, sentValue: User) => boolean;
+type User = { username: string; password: string };
+
+const verifyUser: verifyUserFn = (user, sentValue) => {
+  return (
+    user.username === sentValue.username && user.password === sentValue.password
+  );
+};
+
+
+const bdUser = {username: 'João', password: '12345'};
+const sentUser = {username: 'João', password: '12345'};
+
+const logged1 = verifyUser(bdUser, sentUser);
+
+console.log(logged1);
