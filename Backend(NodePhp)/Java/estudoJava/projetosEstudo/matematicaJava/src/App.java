3420a48084d0557a9ac54405146bac30a89a3b7b
Ryan Menezes <ryanoliveiram2015@gmail.com>
2024-03-15 15:09:20 -0300

    Aprendendo sobre operadores lógicos e relacionais

--- Backend(NodePhp)/Java/estudoJava/projetosEstudo/matematicaJava/src/App.java
+++ Backend(NodePhp)/Java/estudoJava/projetosEstudo/matematicaJava/src/App.java
@@ -20,11 +20,18 @@ public class App {
         return numero1 % numero2;
     }
 
+    static void teste(String nome) {
+        char eAcento = 'é';
+        String msg = String.format("Bem-vindo %s %c um prazer te conhecer!", nome, eAcento);
+        System.out.println(msg);
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println(somar(5, 3)); // Soma dois valores
         System.out.println(subtrair(5, 3)); // subtrair dois valores
         System.out.println(multiplicar(5, 3)); // multiplica dois valores
         System.out.println(dividir(5, 1)); // Divide dois valores
         System.out.println(restoDivisao(5, 3)); // Retorna o resto da divisão (quando há resto)
+        teste("Ryan");
     }
 }
