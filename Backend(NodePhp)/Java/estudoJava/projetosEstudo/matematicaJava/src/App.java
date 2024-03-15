721a2910666e94733066a2c64dd0cd4e2b1ae40e
Ryan Menezes <ryanoliveiram2015@gmail.com>
2024-03-15 02:07:54 -0300

    matematica em java

--- Backend(NodePhp)/Java/estudoJava/projetosEstudo/matematicaJava/src/App.java
+++ Backend(NodePhp)/Java/estudoJava/projetosEstudo/matematicaJava/src/App.java
@@ -1,5 +1,30 @@
 public class App {
+
+    static int somar(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    static int subtrair(int numero1, int numero2) {
+        return numero1 - numero2;
+    }
+
+    static int multiplicar(int numero1, int numero2) {
+        return numero1 * numero2;
+    }
+
+    static int dividir(int numero1, int numero2) {
+        return numero1 / numero2;
+    }
+
+    static int restoDivisao(int numero1, int numero2) {
+        return numero1 % numero2;
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(somar(5, 3)); // Soma dois valores
+        System.out.println(subtrair(5, 3)); // subtrair dois valores
+        System.out.println(multiplicar(5, 3)); // multiplica dois valores
+        System.out.println(dividir(5, 1)); // Divide dois valores
+        System.out.println(restoDivisao(5, 3)); // Retorna o resto da divisão (quando há resto)
     }
 }
